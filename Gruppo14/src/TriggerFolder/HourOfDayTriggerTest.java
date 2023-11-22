857df7611c167898337794a2905d6419b1d047df
Gionnybomb10 <g.ommeniello@studenti.unisa.it>
2023-11-22 17:06:13 +0100

    TestHourOfDayTrigger.java

---
+++ Gruppo14/src/TriggerFolder/HourOfDayTriggerTest.java
@@ -0,0 +1,52 @@
+package TriggerFolder;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalTime;
+
+public class HourOfDayTriggerTest {
+
+    
+    @Test
+    public void testCheckTriggerBeforeTargetTime() {
+        LocalTime currentTime = LocalTime.now();
+
+        // Extract the hour and minutes.
+        int currentHour = currentTime.getHour();
+        int currentMinute = currentTime.getMinute();
+
+        //targetTime is greater than currentdate because of +1.
+        HourOfDayTrigger trigger = new HourOfDayTrigger(currentHour, (currentMinute+1));
+        assertFalse(trigger.checkTrigger(), "CheckTrigger should be false before target time");
+    }
+
+     @Test
+    public void testCheckTriggerEqualTime() {
+        // Get the current time
+        LocalTime currentTime = LocalTime.now();
+
+        // Extract the hour and minutes
+        int currentHour = currentTime.getHour();
+        int currentMinute = currentTime.getMinute();
+
+        //targetTime is equal to currentdate.
+        HourOfDayTrigger trigger = new HourOfDayTrigger(currentHour, currentMinute);
+        assertTrue(trigger.checkTrigger(), "CheckTrigger should be false before target time");
+    }
+
+
+    @Test
+    public void testCheckTriggerAfterTargetTime() {
+        // Get the current time.
+        LocalTime currentTime = LocalTime.now();
+
+        // Extract the hour and minutes.
+        int currentHour = currentTime.getHour();
+        int currentMinute = currentTime.getMinute();
+
+        //targetTime is less than currentdate because of -1.
+        HourOfDayTrigger trigger = new HourOfDayTrigger(currentHour, (currentMinute-1));
+        assertTrue(trigger.checkTrigger(), "CheckTrigger should be true after target time");
+    }
+}
