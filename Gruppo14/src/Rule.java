623d1372648668eebf1f66afd557b526451dedc6
emanuele-02 <e.nicolo@studenti.unisa.it>
2023-11-22 17:37:32 +0100

    Modifiche classe Rule

--- Gruppo14/src/Rule.java
+++ Gruppo14/src/Rule.java
@@ -1,31 +1,57 @@
-import java.lang.Thread.State;
-import java.sql.Time;
-import java.util.LinkedList;
-import java.util.List;
-
+import java.time.Duration;
 import ActionFolder.Action;
 import TriggerFolder.Trigger;
 
-public class Rule extends RuleSubject{
+public class Rule extends RuleSubject implements Runnable{
     
     private String ruleName;
     private Trigger trigger;
-    private List<Action> action;
-    private boolean isActive;
-    private Time triggerTime;
-    private State ruleState;
+    private Action action;
+    private boolean isOneTimeTrigger;
+    private Duration timeDuration;
+    
+    /*costructor in case timeDuration is not specified. It will be set to a number of days pair to the maximum value of long */
+    public Rule(String ruleName, Trigger trigger, Action action, boolean isOneTimeTrigger) {
 
-    public Rule(Trigger trigger, List<Action> action) {
-
+        this.ruleName=ruleName;
         this.trigger = trigger;
         this.action = action;
-        this.action= new LinkedList<>();
+        this.isOneTimeTrigger=isOneTimeTrigger;
+        this.timeDuration=Duration.ofDays(Long.MAX_VALUE);
 
     }
 
+    /*costructor in case timeDuration is specified. It will be set to a specified number of second*/
+    public Rule(String ruleName, Trigger trigger, Action action, boolean isOneTimeTrigger, Duration timeDuration) {
+
+        this.ruleName=ruleName;
+        this.trigger = trigger;
+        this.action = action;
+        this.timeDuration= timeDuration;
+        this.isOneTimeTrigger=isOneTimeTrigger;
+
+    }
+
+    /*method to activate a Rule */
+    public void activate(RuleObserver observer) {
+    
+        super.attach(observer); 
+
+    }
+
+    /*method to deactivate a Rule */
+    public void deactivate() {
+
+        super.detach();
+
+    }
 
     
+    @Override
+    public void run() {
 
-
+        
+        
+    }
 
 }
