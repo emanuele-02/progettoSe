9a1a2dcb04088250f31b3194c66d464439714d1d
emanuele-02 <e.nicolo@studenti.unisa.it>
2023-11-23 11:02:21 +0100

    Modifiche di Rule

--- Gruppo14/src/Rule.java
+++ Gruppo14/src/Rule.java
@@ -2,59 +2,98 @@ import java.time.Duration;
 import ActionFolder.Action;
 import TriggerFolder.Trigger;
 
-public class Rule extends RuleSubject implements Runnable{
+public class Rule implements Runnable{
     
     private String ruleName;
     private Trigger trigger;
     private Action action;
-    private boolean isOneTimeTrigger;
-    private Duration timeDuration;
+    private Duration timeDelay;
+    private boolean isActive;
+
     
-    /*costructor in case timeDuration is not specified. It will be set to a number of days pair to the maximum value of long */
-    public Rule(String ruleName, Trigger trigger, Action action, boolean isOneTimeTrigger) {
+    /*costructor in case timeDuration is not specified. It will be set to a number of days pair to the maximum value of long. When the rule is created, it will be added within the rule list */
+    public Rule(String ruleName, Trigger trigger, Action action) {
 
         this.ruleName=ruleName;
         this.trigger = trigger;
         this.action = action;
-        this.isOneTimeTrigger=isOneTimeTrigger;
-        this.timeDuration=Duration.ofDays(Long.MAX_VALUE);
+        this.isActive=true;
+        this.timeDelay=Duration.ZERO;
+        RuleManager ruleManager=RuleManager.getInstance();
+        ruleManager.addRule(this);
 
     }
 
-    /*costructor in case timeDuration is specified. It will be set to a specified number of second*/
-    public Rule(String ruleName, Trigger trigger, Action action, boolean isOneTimeTrigger, Duration timeDuration) {
+    /*costructor in case timeDuration is specified. It will be set to a specified number of second. When the rule is created, it will be added within the rule list*/
+    public Rule(String ruleName, Trigger trigger, Action action, long daysOfDelay, long hoursOfDelay, long minutesOfDelay) {
 
         this.ruleName=ruleName;
         this.trigger = trigger;
         this.action = action;
-        this.timeDuration= timeDuration;
-        this.isOneTimeTrigger=isOneTimeTrigger;
+        this.isActive= true;
+        this.timeDelay= Duration.ofDays(daysOfDelay).plusHours(hoursOfDelay).plusMinutes(minutesOfDelay);
+        RuleManager ruleManager=RuleManager.getInstance();
+        ruleManager.addRule(this);
+
+
+    }
+
+    /*method get on the rule's state */
+    public boolean isActive(){
+
+        return this.isActive;
 
     }
 
     /*method to activate a Rule */
-    public void activate(RuleObserver observer) {
-    
-        super.attach(observer); 
+    public void activate() {
+           
+        isActive=true;
 
     }
 
     /*method to deactivate a Rule */
     public void deactivate() {
 
-        super.detach();
-
+        isActive=false;
+  
     }
 
-    
+    public String getRuleName(){
+
+        return this.ruleName;
+    }
+
+    public void setRuleName(String newName){
+
+        this.ruleName=newName;
+    }
+
     @Override
     public void run() {
-    //Costruttore vuoto per i test
-    
+        
+        while(isActive){
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                
+                System.err.println("Error in rule's execution\t");
+                e.printStackTrace();
+            }
 
-        
-        
+            if(trigger.checkTrigger()){
+
+                action.execute();
+
+                try {
+                    Thread.sleep(this.timeDelay.toMillis());
+                } catch (InterruptedException e) {
+                    System.err.println("Error in rule's execution\t");
+                    e.printStackTrace();
+                }
+            }
+
+        }
     }
-
 
 }
