0194f57a91387164ae13bcfcb7f11eac7fbe7873
Frk2208 <f.pappalardo20.unisa.it>
2023-11-23 10:36:49 +0100

    RuleManager remove confirmation + modify Test

--- Gruppo14/src/Rule.java
+++ Gruppo14/src/Rule.java
@@ -1,33 +1,89 @@
-import java.lang.Thread.State;
-import java.sql.Time;
-import java.util.LinkedList;
-import java.util.List;
-
+import java.time.Duration;
 import ActionFolder.Action;
 import TriggerFolder.Trigger;
 
-public class Rule extends RuleSubject{
+public class Rule implements Runnable{
     
     private String ruleName;
     private Trigger trigger;
-    private List<Action> action;
+    private Action action;
+    private Duration timeDuration;
     private boolean isActive;
-    private Time triggerTime;
-    private State ruleState;
 
-    public Rule(Trigger trigger, List<Action> action) {
+    
+    /*costructor in case timeDuration is not specified. It will be set to a number of days pair to the maximum value of long. When the rule is created, it will be added within the rule list */
+    public Rule(String ruleName, Trigger trigger, Action action) {
 
+        this.ruleName=ruleName;
         this.trigger = trigger;
         this.action = action;
-        this.action= new LinkedList<>();
+        this.isActive=true;
+        //this.timeDuration=Duration.ofDays(Long.MAX_VALUE);
+        RuleManager ruleManager=RuleManager.getInstance();
+        ruleManager.addRule(this);
 
     }
 
-    //Costruttore vuoto per i test
-    public Rule(){
+    /*costructor in case timeDuration is specified. It will be set to a specified number of second. When the rule is created, it will be added within the rule list*/
+    public Rule(String ruleName, Trigger trigger, Action action, Duration timeDuration) {
+
+        this.ruleName=ruleName;
+        this.trigger = trigger;
+        this.action = action;
+        this.isActive= true;
+        //this.timeDuration= timeDuration;
+        RuleManager ruleManager=RuleManager.getInstance();
+        ruleManager.addRule(this);
+
 
     }
 
+    /*method get on the rule's state */
+    public boolean isActive(){
 
+        return this.isActive;
+
+    }
+
+    /*method to activate a Rule */
+    public void activate() {
+           
+        isActive=true;
+
+    }
+
+    /*method to deactivate a Rule */
+    public void deactivate() {
+
+        isActive=false;
+  
+    }
+
+    @Override
+    public void run() {
+        
+        long startTime=System.currentTimeMillis();
+
+        long finishTime=startTime+ timeDuration.toMillis();
+
+        while(System.currentTimeMillis()<finishTime){
+            while(isActive){
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    
+                    System.out.println("Errore nell'esecuzione della regola:\t");
+                    e.printStackTrace();
+                }
+
+                if(trigger.checkTrigger()){
+
+                    action.execute();
+                }
+
+            }
+
+        }
+    }
 
 }
