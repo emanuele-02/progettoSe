011632de7853410ffe9672720838aece4ea60d42
Frk2208 <f.pappalardo20.unisa.it>
2023-11-23 11:25:57 +0100

    rename in RuleManagerTest + comment adjustment

--- Gruppo14/src/TriggerFolder/HourOfDayTrigger.java
+++ Gruppo14/src/TriggerFolder/HourOfDayTrigger.java
@@ -2,16 +2,16 @@ package TriggerFolder;
 
 import java.time.LocalTime;
 
-// Implementazione della classe HourOfDayTrigger che implementa l'interfaccia Trigger
+
 public class HourOfDayTrigger implements Trigger {
     private LocalTime targetTime;
 
-    // Costruttore
+
     public HourOfDayTrigger(int hour, int minute) {
         this.targetTime = LocalTime.of(hour, minute);
     }
 
-    // Metodo per verificare se la regola deve essere attivata
+
     @Override
     public boolean checkTrigger() {
         LocalTime currentTime = LocalTime.now();
