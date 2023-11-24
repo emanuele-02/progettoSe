3138e5a3945173d16fc8de2ef615b2c1b48a56e3
emanuele-02 <e.nicolo@studenti.unisa.it>
2023-11-24 16:04:06 +0100

    Modifiche delle classi Rule e RuleTest

--- Gruppo14/src/RuleTest.java
+++ Gruppo14/src/RuleTest.java
@@ -4,57 +4,32 @@ import org.junit.jupiter.api.Test;
 import ActionFolder.*;
 import TriggerFolder.*;
 
-
 public class RuleTest {
 
     private Trigger t= new HourOfDayTrigger(3, 4);
     private Action a= new DialogBoxAction("Hello, World");
-    private Rule r= new Rule("Prova", t, a, 0, 0, 100);
-    
+    private Rule r= new Rule("Prova", t, a);
+
+
     @Test
     void testActivate() {
 
+        r.deactivate();;
         r.activate();
 
         assertTrue(r.isActive());
 
     }
-
-
     @Test
     void testDeactivate() {
 
-        r.deactivate();
-
-        assertFalse(r.isActive());
-
-
-    }
-
-    @Test
-    void testIsActive() {
-
         r.activate();
-        assertTrue(r.isActive());
-
         r.deactivate();
         assertFalse(r.isActive());
         
-
     }
+    
 
-    @Test
-    void testRun() throws InterruptedException {
 
-        Thread thread= new Thread(r);
-        thread.start();
-
-        Thread.sleep(1000);
-
-        assertTrue(t.checkTrigger());
-
-        r.deactivate();
-
-        thread.join();
-    }
+    
 }
