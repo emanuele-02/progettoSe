27841997039285bc7f879bca8a2ec246ba542573
emanuele-02 <e.nicolo@studenti.unisa.it>
2023-11-24 16:28:31 +0100

    Rule Manager modificato

--- Gruppo14/src/RuleManager.java
+++ Gruppo14/src/RuleManager.java
@@ -1,28 +1,45 @@
-import java.io.ByteArrayInputStream;
+import java.util.Scanner;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
-public class RuleManager implements RuleObserver {
+public class RuleManager {
     private static RuleManager instance;
     private List<Rule> ruleList;
+    private ScheduledExecutorService scheduler;
 
-    // costrutture della classe
-     private RuleManager() {
+    private RuleManager() {
         ruleList = new ArrayList<>();
+        scheduler = Executors.newSingleThreadScheduledExecutor();
+        scheduleRuleEvaluation();
     }
-    // metoto get istance
+
     public static synchronized RuleManager getInstance() {
         if (instance == null) {
             instance = new RuleManager();
         }
         return instance;
     }
+
+
     protected void addRule(Rule rule) {
+        if (rule == null) {
+            throw new IllegalArgumentException("Rule cannot be null");
+        }
         ruleList.add(rule);
     }
 
     public void removeRule(Rule rule) {
+        if (rule == null) {
+            throw new IllegalArgumentException("Rule cannot be null");
+        }
+
+        if (!ruleList.contains(rule)) {
+            throw new IllegalArgumentException("Rule not found in the rule list");
+        }
+
         // Ask for confirmation before removing the rule
         if (confirmRemoval()) {
             ruleList.remove(rule);
@@ -32,23 +49,36 @@ public class RuleManager implements RuleObserver {
         }
     }
 
-    // Method to get confirmation from the user(no case sensitive)
     private boolean confirmRemoval() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Are you sure you want to remove the rule? (Yes/No): ");
-        String response = scanner.nextLine().trim().toLowerCase();
-        return response.equals("yes") || response.equals("y");
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Are you sure you want to remove the rule? (Yes/No): ");
+            String response = scanner.nextLine().trim().toLowerCase();
+            return response.equals("yes") || response.equals("y");
+        }
     }
 
-    //for testing might come in handy in the future
     public List<Rule> getRuleList() {
-        return new ArrayList<>(ruleList); // Return a copy to prevent external modification
+        return new ArrayList<>(ruleList);
     }
 
+    //logic of evaluation and execution of rules
+    private void evaluateRules() {
+        for (Rule rule : ruleList) {
+            if(rule.getTrigger().checkTrigger() && rule.isActive()){
+                rule.getAction().execute();
+            }
+        }
+    }
 
-    @Override
-    public void updateRule() {
-       
-        throw new UnsupportedOperationException("Unimplemented method 'updateRule'");
+    //scheduleAtFixedRate schedules the periodic execution of a task
+    private void scheduleRuleEvaluation() {
+        //this::evaluateRules is a lambda expression representing the task to be executed
+        //param 0: initial delay time param 1: time interval between executions
+        scheduler.scheduleAtFixedRate(this::evaluateRules, 0, 5, TimeUnit.SECONDS);
+    }
+
+    //close scheduler
+    public void shutdown() {
+        scheduler.shutdown();
     }
 }
