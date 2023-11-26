e602a74512b026695742c13999de5eb16581fea0
Frk2208 <f.pappalardo20.unisa.it>
2023-11-26 16:34:36 +0100

    RuleManager remove scanner

--- Gruppo14/src/App.java
+++ Gruppo14/src/App.java
@@ -1,75 +1,168 @@
-import java.time.LocalTime;
-
 import ActionFolder.*;
 import TriggerFolder.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 public class App {
 
+
     public static void main(String[] args) {
-        // Creation of 2 actions
-        ActionFolder.Action action = new DialogBoxAction("Pippo");
-        ActionFolder.Action action2 = new AudioAction("TestAudioAction.wav");
-
-        // Trigger creation (always True)
-        LocalTime currentTime = LocalTime.now();
-        int currentHour = currentTime.getHour();
-        int currentMinute = currentTime.getMinute();
-        TriggerFolder.Trigger trigger = new HourOfDayTrigger(currentHour, currentMinute);
+        Scanner scanner = new Scanner(System.in);
+        RuleManager rules= RuleManager.getInstance();
+        Map<String,Trigger> triggers= new HashMap<>();
+        Map<String,Action> actions= new HashMap<>();
         
-        // Creation of 2 Rules
-        Rule rule = new Rule("Reminder", trigger, action);
-        Rule rule2 = new Rule("Reminder2", trigger, action2);
-        
-        RuleManager ruleManager = RuleManager.getInstance();
+        while (true) {
+            System.out.println("1. Create a Rule (remember that you have to create a trigger and an action for this rule before creating it)");
+            System.out.println("2. Create a Trigger");
+            System.out.println("3. Create an Action");
+            System.out.println("4. View existing rules");
+            System.out.println("5. Remove a Rule");
+            System.out.println("6. Exit");
 
-        // View all set of rules
-        System.out.println("Set of Rule:");
-        ruleManager.getRuleList().forEach(r -> System.out.println(r.getRuleName()));
+            int choice = scanner.nextInt();
+            scanner.nextLine();  
 
-        // Wait 11s to allow the rules to be executed periodically.
-        try {
-            Thread.sleep(8000); 
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            switch (choice) {
+                case 1:
+                    Rule rule = createRule(triggers, actions, scanner);
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    rule.deactivate();
+                    break;
+
+                case 2:
+                    createTrigger(triggers, scanner);
+                    break;
+
+                case 3:
+                    createAction(actions, scanner);
+                    break;
+
+                case 4:
+                    displayRules(rules);
+                    break;
+
+                case 5:
+                    removeRule(rules, scanner);
+                    break;
+
+                case 6:
+                    System.out.println("Bye!");
+                    rules.getRuleList().clear();
+                    rules.shutdown();
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Invalid choice, retry");
+            }
+        }
+    }
+
+    private static Rule createRule(Map<String,Trigger> triggers, Map<String,Action> actions, Scanner scanner) {
+
+        System.out.println("Enter rule's name");
+        String ruleName=scanner.nextLine();
+
+        System.out.println("Enter the name of the trigger you want to trigger the rule");
+        String triggerName = scanner.nextLine();
+        Trigger t= triggers.get(triggerName);
+
+        System.out.println("Enter the name of the action you want to be performed");
+        String actionName = scanner.nextLine();
+        Action a= actions.get(actionName);
+    
+        return new Rule(ruleName, t, a);
+    }
+
+    private static void createTrigger(Map<String, Trigger> triggers, Scanner scanner){
+
+        System.out.println("Insert trigger's name");
+        String name= scanner.nextLine();
+
+        System.out.println("Insert trigger's hour");
+        int hour= scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Insert trigger's minute");
+        int minute= scanner.nextInt();
+        scanner.nextLine();
+
+        Trigger t= new HourOfDayTrigger(hour, minute);
+
+        triggers.putIfAbsent(name, t);
+
+    }
+
+    private static void createAction (Map<String, Action> actions, Scanner scanner){
+
+        System.out.println("Insert action's name");
+        String name= scanner.nextLine();
+        System.out.println("Which type of action do you want create?");
+        System.out.println("1. AudioAction");
+        System.out.println("2. DialogBoxAction");
+
+        int choice= scanner.nextInt();
+        scanner.nextLine();
+
+        switch(choice){
+
+            case 1:
+                System.out.println("Insert the audio's path");
+                String path= scanner.nextLine();
+                Action audioAction= new AudioAction(path);
+                actions.putIfAbsent(name, audioAction);
+                break;
+
+            case 2:
+                System.out.println("Insert the message");
+                String message= scanner.nextLine();
+                Action dialogAction= new DialogBoxAction(message);
+                actions.putIfAbsent(name, dialogAction);
+                break;
+
+            default:
+                System.out.println("Invalid choice, retry");
+
         }
 
-        // Rules disactivation
-        System.out.println("Rules deactivated......");
-        rule.deactivate();
-        rule2.deactivate();
+    }
+
+    private static void displayRules(RuleManager rules) {
+
+        List<Rule> ruleList= rules.getRuleList();
+
+        if (ruleList.isEmpty()) 
+            System.out.println("No rule present");
         
-        try {
-            Thread.sleep(10000); 
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        else {
+
+            System.out.println("Existing rules");
+            for (Rule rule : ruleList) {
+                System.out.println(rule.getRuleName());
+            }
+
+        }
+    }
+
+    private static void removeRule(RuleManager rules, Scanner scanner){
+
+        System.out.println("Enter the name of the rule you wish to delete");
+        String name=scanner.nextLine();
+
+        for(Rule r : rules.getRuleList()){
+
+            if(name.equalsIgnoreCase(r.getRuleName()))
+                rules.removeRule(r, scanner);
+                
         }
 
-        // Reactivating first rule
-        System.out.println("Reactivation rule Reminder(Pippo)...");
-        rule.activate();
-
-        // 1 execution of rule 1
-        try {
-            Thread.sleep(5000); 
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // Rule deactivated again
-        System.out.println("Deactivation rule Reminder(Pippo)...");
-        rule.deactivate();
-
-        //Remove rule 
-        System.out.println("Remove rule Reminder(Pippo)...");
-        ruleManager.removeRule(rule);
-
-
-        // Set of rule after remove
-        System.out.println("Set of rules after removal:");
-        ruleManager.getRuleList().forEach(r -> System.out.println(r.getRuleName()));
-
-        
-        ruleManager.getRuleList().clear();
-        ruleManager.shutdown();
     }
 }
