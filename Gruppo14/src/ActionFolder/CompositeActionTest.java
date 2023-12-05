f99034a7c5e0fe30387c54177a3d69db4aa14d62
Frk2208 <f.pappalardo20.unisa.it>
2023-12-05 10:25:08 +0100

    composite action +enum logicaloperetion

---
+++ Gruppo14/src/ActionFolder/CompositeActionTest.java
@@ -0,0 +1,42 @@
+package ActionFolder;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CompositeActionTest {
+
+    private CompositeAction actions;
+    private DialogBoxAction a,a1;
+
+    @BeforeEach
+    public void setUp() {
+        actions = new CompositeAction();
+        a = new DialogBoxAction("pippo");
+        a1 = new DialogBoxAction("pluto");
+    }
+
+    @Test
+    void addExecuteMultipleTest(){
+        // Expected messagge pippo,pluto
+        actions.addAction(a);
+        actions.addAction(a1);
+        actions.execute();
+    }
+
+    @Test
+    void removeTest() {
+        // Expected message pluto
+        actions.addAction(a);
+        actions.addAction(a1);
+        actions.removeAction(a);
+        actions.execute();
+    }
+
+    @Test
+    void removeEmptyTest() {
+        assertThrows(IllegalStateException.class, () -> actions.removeAction(a));
+    }
+
+}
